22cc57229d5c1a8c61beebc2baff5c876784f696
yasmine kordjazy <ykordjaz@gmail.com>
2021-11-08 16:55:18 +0100

    done

---
+++ config/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def about
+  end
+
+  def ask
+  end
+
+  def answer
+    # raise
+
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].last == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+
+  end
+end
